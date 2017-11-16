841e662c1a300d17e90f18fa564744871a79bcc1
Jiahao Dai <hacka@djh.im>
2017-11-16 14:44:24 +0800

    Feature to import config

--- js/content-heavy-left.js
+++ js/content-heavy-left.js
@@ -1,19 +1,59 @@
-window.onload = function() {
+window.addEventListener('load', configHelperFeed43, false);
+
+function configHelperFeed43() {
     console.log('feed43.com config helper loaded');
     chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
-        let feed43 = {};
-        feed43.url = document.getElementById('url').value;
-        feed43.encoding = document.getElementById('encoding').value;
-        feed43.global_pattern = document.getElementById('global_pattern').value
-        feed43.item_pattern = document.getElementById('item_pattern').value
+        if (msg.codename === "get") {
+            let feed43 = {};
+            feed43.url = document.getElementById('url').value;
+            feed43.encoding = document.getElementById('encoding').value;
+            feed43.global_pattern = document.getElementById('global_pattern').value;
+            feed43.item_pattern = document.getElementById('item_pattern').value;
 
-        feed43.feed_title = document.getElementById('feed_title').value;
-        feed43.feed_link = document.getElementById('feed_link').value;
-        feed43.feed_description = document.getElementById('feed_description').value;
-        feed43.item_title = document.getElementById('item_title').value;
-        feed43.item_link = document.getElementById('item_link').value;
-        feed43.item_template = document.getElementById('item_template').value;
+            feed43.feed_title = document.getElementById('feed_title').value;
+            feed43.feed_link = document.getElementById('feed_link').value;
+            feed43.feed_description = document.getElementById('feed_description').value;
+            feed43.item_title = document.getElementById('item_title').value;
+            feed43.item_link = document.getElementById('item_link').value;
+            feed43.item_template = document.getElementById('item_template').value;
 
-        sendResponse(feed43);
+            sendResponse(feed43);
+        } else {
+            let feed43 = msg.config;
+            if (feed43.url !== undefined) {
+                document.getElementById('url').value = feed43.url;
+            }
+            if (feed43.encoding !== undefined) {
+                document.getElementById('encoding').value = feed43.encoding;
+            }
+            if (feed43.global_pattern !== undefined) {
+                document.getElementById('global_pattern').value = feed43.global_pattern;
+            }
+            if (feed43.item_pattern !== undefined) {
+                document.getElementById('item_pattern').value = feed43.item_pattern;
+            }
+            if (feed43.feed_title !== undefined) {
+                document.getElementById('feed_title').value = feed43.feed_title;
+            }
+            if (feed43.feed_link !== undefined) {
+                document.getElementById('feed_link').value = feed43.feed_link;
+            }
+            if (feed43.feed_description !== undefined) {
+                document.getElementById('feed_description').value = feed43.feed_description;
+            }
+            if (feed43.item_title !== undefined) {
+                document.getElementById('item_title').value = feed43.item_title;
+            }
+            if (feed43.item_link !== undefined) {
+                document.getElementById('item_link').value = feed43.item_link;
+            }
+            if (feed43.item_template !== undefined) {
+                document.getElementById('item_template').value = feed43.item_template;
+            }
+
+            sendResponse({
+                "result": "Success!"
+            });
+        }
     });
 };
--- js/index.js
+++ js/index.js
@@ -7,28 +7,42 @@ function setTabFocus() {
 
     if (this.id === 'btn-export') {
         document.getElementById('btn-submit').value = 'Get config';
+        document.getElementById('textarea').placeholder = '';
     } else {
         document.getElementById('btn-submit').value = 'Apply';
+        document.getElementById('textarea').placeholder = 'Paste JSON style config here...';
     }
 }
 
 function heavy_left() {
-    if (document.getElementById('btn-submit').value.indexOf('Get') !== -1) {
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function(tabs) {
-            var tab = tabs[0];
-            chrome.tabs.getSelected(null, function(tab) {
-                chrome.tabs.sendMessage(tab.id, {}, function(msg) {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        var tab = tabs[0];
+        chrome.tabs.getSelected(null, function(tab) {
+            let _obj_msg = {};
+            let _obj_config = {};
+            if (document.getElementById('btn-submit').value.indexOf('Get') !== -1) {
+                _obj_msg = {
+                    "codename": "get"
+                };
+            } else {
+                _obj_msg = {
+                    "codename": "apply"
+                };
+                _obj_config = JSON.parse(document.getElementById('textarea').value);
+                _obj_msg.config = _obj_config;
+            }
+            chrome.tabs.sendMessage(tab.id, _obj_msg, function(msg) {
+                if (document.getElementById('btn-submit').value.indexOf('Get') !== -1) {
                     document.getElementById('textarea').value = JSON.stringify(msg, null, 4);
-                    console.log('onResponse', msg);
-                });
+                } else {
+                    document.getElementById('btn-submit').value = msg.result;
+                }
             });
         });
-    } else {
-        alert('not implimented');
-    }
+    });
 }
 
 
